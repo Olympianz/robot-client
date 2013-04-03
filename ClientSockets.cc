6acb4610c16b77cc3d2fe1f472b99b28b8d10722
olympianz <yue_wu_di@hotmail.com>
2013-04-03 00:49:08 -0400

    working on output

--- ClientSockets.cc
+++ ClientSockets.cc
@@ -10,20 +10,18 @@
  * robotic client
  */
 
-using std::vector;      
+using std::vector;      using std::cout;
+using std::endl;
 
-ClientSockets::ClientSockets (  std::vector<ArClientBase *>& clientBases,
-                                std::vector<Arsocket *>& clSockets)
-                                : v_clients(clientBases), v_socks(clSockets){}
+ClientSockets::ClientSockets (  vector<ArClientBase *>& clientBases,
+                                const vector<HostInfo>& hinfo) {
+    hostsinfo = hinfo;
 
-void ClientSockets::getSockets(vector<ArClientBase *>& clients){
-    typedef vector<ArclientBase *>::size_type Arcb_size;
-    
-    ArSocket* socket = NULL;
+       typedef vector<ArClientBase *>::size_type clt_sz;
 
-    for ( Arcb_size i = 0; i != clients.size(); ++i){
-        socket = clients[i].getUdpSocket();
-        v_socks.push_back(socket);
+    for ( clt_sz i = 0; i != clientBases.size(); ++i){
+        v_socks.push_back( clientBases[i].getUdpSocket() );
+        v_clients.push_back( clientBases[i] );
     }
 }
 
@@ -32,11 +30,27 @@ vector<long>& ClientSockets::getData(vector<long>& trafData){
 
     for ( sock_sz i = 0; i != v_sockes.size(); ++i)
         trafData.push_back(v_sockes[i]->getBytesRecvd());
-
     return trafData;
 }
 
+vector<long> ClientSockets::getData() {
+    typedef vector<ArSocket *>::size_type sock_sz;
+    vector<long> temp;
 
+    for ( sock_sz i = 0; i != v_sockes.size(); ++i)
+        temp.push_back(v_sockes[i]->getBytesRecvd());
+    return temp;
+}
+
+// need to define a data class for storing robot name and data
+void ClientSockets::printData () {
+    typedef vector<long>::size_type lsize;
+    vector<long> temp = getData();
+
+    for ( lsize i = 0; i != temp.size(); ++i){
+         
+
+}
     
 
 
--- ClientSockets.h
+++ ClientSockets.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "Aria.h"
+#include "ConfigFileReader.h"
 
 /* This class aims to monitor and log the traffic 
  * between robot servers and client by reading the 
@@ -12,19 +13,18 @@
 
 class ClientSockets {
     public:
-        ClientSockets(  std::vector<ArClientBase *>& clientBases, 
-                        std::vector<Arsocket *>& clSockets = NULL);
-        std::vector<long>& getData (std::vector<long>&);   
+        ClientSockets(  std::vector<ArClientBase *>&, const std::vector<HostInfo>& );
+
+        std::vector<long>& getData (std::vector<long>&);
+        std::vector<long>  getData ();
+
+        void printData ();
+
     private:
-        std::vector<ArClientBase *> v_clients;
-        std::vector<ArSocket *> v_socks;
-        void getSockets (std::vector<ArClientBase *>&);
+        std::vector<ArSocket *> v_socks; 
+        const std::vector<ArClientBase *>& v_clients;
+        const std::vector<HostInfo>& hostsinfo; 
 };
-
-
-
-
-
 
 
 #endif
